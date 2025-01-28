5f9e4098b2d889c9e976b41ff950ad6fd3777b0a
webculcate <webculcate@gmail.com>
2025-01-28 17:49:43 +0530

    added event service core

---
+++ event-service-core/src/main/java/com/webculcate/eventservicecore/EventServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.webculcate.eventservicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceCoreApplication.class, args);
+	}
+
+}
---
+++ event-service-core/src/test/java/com/webculcate/eventservicecore/EventServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webculcate.eventservicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
